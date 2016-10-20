5a6c048bd3b4374350ea69435e8f20289c1a608b
ao1415 <taku31126816@yahoo.co.jp>
2016-10-20 12:53:39 +0900

    評価関数をクラスに移行

--- CodevsForStudent/AI.hpp
+++ CodevsForStudent/AI.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Evaluation.hpp"
 #include "Simulator.hpp"
 
 class AI {
@@ -64,10 +65,14 @@ public:
 							{
 								hashSet.insert(hash);
 
-								ScoreBoard scoreBoard;
-								top.score += eval(top, score, scoreBoard);
+								//ScoreBoard scoreBoard;
+								//top.score += eval(top, score, scoreBoard);
 								//top.score = eval(top, score, scoreBoard);
-								top.scoreBoard.emplace_back(scoreBoard);
+
+								Evaluation evaluation;
+								top.score += evaluation.getScore(top.stage, top.obstacle, score, turn);
+
+								top.scoreBoard.emplace_back(evaluation);
 
 								top.obstacle = obstacle;
 								if (top.command.empty()) top.command = toCommand(pos, r);
@@ -97,226 +102,15 @@ public:
 
 private:
 
-	struct ScoreBoard {
-		int obstacleScore = 0;
-		int linkScore = 0;
-
-		int flatScore = 0;
-		int average = 0;
-		int heightError = 0;
-		int triggerHeight = 0;
-
-		int obstacle = 0;
-
-		int maxScore = 0;
-		int attackScore = 0;
-		int trashScore = 0;
-
-		int score = 0;
-
-		int waitNumber = 0;
-		Point point;
-
-		void show() const {
-			cerr << "お邪魔配置スコア\t:" << obstacleScore << endl;
-			cerr << "結合スコア\t\t:" << linkScore << endl << endl;
-
-			cerr << "凹凸スコア\t\t:" << flatScore << endl;
-			cerr << "ブロックの高さ平均\t:" << average << endl;
-			cerr << "U字形状スコア\t\t:" << heightError << endl;
-			cerr << "発火位置\t\t:" << triggerHeight << endl << endl;
-
-			cerr << "お邪魔ブロック数\t:" << obstacle << endl << endl;
-
-			cerr << "想定連鎖スコア\t\t:" << maxScore << endl;
-			cerr << "発火連鎖スコア\t\t:" << attackScore << endl << endl;
-
-			cerr << "連鎖尾スコア\t\t:" << trashScore << endl << endl;
-
-			cerr << "総合スコア\t\t:" << score << endl;
-			cerr << point << ", wait:" << waitNumber << endl;
-		}
-
-	};
-
 	struct Data {
 		StageArray stage;
 		int obstacle;
 		int score;
 		string command;
 
-		vector<ScoreBoard> scoreBoard;
+		vector<Evaluation> scoreBoard;
 
 		const bool operator<(const Data& d) const { return score < d.score; }
 	};
-
-	const int eval(const Data& data, const int attackScore, ScoreBoard& scoreBoard) const {
-
-		array<int, StageWidth> blockTop;
-
-		int linkScore = 0;
-		int obstacleScore = 0;
-		for (int x = 0; x < data.stage.getWidth(); x++)
-		{
-			for (int y = data.stage.getHeight() - 1; y >= 0; y--)
-			{
-				//ブロックの天辺の検索
-				if (data.stage[y][x] == EmptyBlock)
-				{
-					blockTop[x] = data.stage.getHeight() - y - 1;
-					break;
-				}
-				//お邪魔ブロックの検索
-				else if (data.stage[y][x] == ObstacleBlock)
-				{
-					obstacleScore += data.stage.getHeight() - y;
-				}
-
-				//右, 左下, 下, 右下
-				const Point direction[] = { Point(1,0),Point(-1,1),Point(0,1),Point(1,1) };
-
-				//ブロックの10以下の連結の検索
-				for (const auto& dire : direction)
-				{
-					int add = data.stage[y][x];
-					Point p = Point(x, y);
-
-					//探査
-					for (int i = 1; i < StageHeight; i++)
-					{
-						p += dire;
-
-						if (!inside(p) || data.stage[p] == 0) break;
-
-						add += data.stage[p];
-
-						if (add < AddScore)
-						{
-							linkScore += 5;
-						}
-						else
-							break;
-					}
-				}
-			}
-		}
-
-		int average = 0;
-		for (const auto& v : blockTop) average += v;
-		average /= (int)blockTop.size();
-
-		//山谷の検索・形の評価
-		int flatScore = 0;
-		int heightError = 0;
-		for (int x = 0; x < (int)blockTop.size(); x++)
-		{
-			const int l = ((x - 1 >= 0) ? blockTop[x - 1] : 99999);
-			const int r = ((x + 1 < (int)blockTop.size()) ? blockTop[x + 1] : 99999);
-
-			const int lsub = blockTop[x] - l;
-			const int rsub = blockTop[x] - r;
-
-			if (lsub >= 4 && rsub >= 4)
-				flatScore -= 1000;
-
-			if (lsub <= -4 && rsub <= -4)
-				flatScore -= 1000;
-
-			const int h = (x - 5)*(x - 5) / 5 + average - 2;
-			const int sub = h - blockTop[x];
-			heightError += sub*sub;
-		}
-		heightError = int(sqrt(heightError));
-
-		Simulator simulator;
-
-		//左上・右上・左・右・左下・下・右下
-		const Point direction[] = { Point(-1,-1),Point(1,-1),Point(-1,0),Point(1,0),Point(-1,1),Point(0,1),Point(1,1) };
-		//連鎖の検索
-		int maxScore = 0;
-		int triggerHeight = 0;
-		StageArray maxStage;
-		for (int x = 0; x < (int)blockTop.size(); x++)
-		{
-			//for (int y = 0; y < PackSize; y++)
-			for (int y = 0; y < PackSize; y++)
-			{
-				const Point point = Point(x, data.stage.getHeight() - blockTop[x] - 1 - y);
-				for (const auto& dire : direction)
-				{
-					Point p = point;
-					int num = AddScore;
-
-					for (int i = 1; i < StageHeight; i++)
-					{
-						p += dire;
-						if (!inside(p) || data.stage[p] == EmptyBlock) break;
-
-						num -= data.stage[p];
-						if (num <= 0) break;
-
-						int s;
-						StageArray checkStage = data.stage;
-						checkStage[point] = num;
-						simulator.next(checkStage, s);
-						if (maxScore < s)
-						{
-							maxScore = s;
-							triggerHeight = abs(blockTop[x] + y - average);
-							maxStage = move(checkStage);
-							scoreBoard.waitNumber = num;
-							scoreBoard.point = point;
-						}
-					}
-
-				}
-			}
-		}
-		int trashScore = 0;
-		for (int y = 0; y < maxStage.getHeight(); y++)
-		{
-			for (int x = 0; x < maxStage.getWidth(); x++)
-			{
-				if (maxStage[y][x] != EmptyBlock && maxStage[y][x] != ObstacleBlock)
-					trashScore++;
-			}
-		}
-
-		int score = 0;
-
-		score -= obstacleScore;
-		score += linkScore;
-
-		score += flatScore;
-		score -= heightError * 5;
-		score -= triggerHeight * 100;
-
-		score -= data.obstacle * 10;
-
-		score += maxScore * 10;
-		//相手と自分の状況をみて閾値を決める
-		score += attackScore < 250 ? attackScore * 10 : attackScore * 20;
-
-		score -= maxScore < 250 ? 0 : trashScore * 3;
-
-		scoreBoard.obstacleScore = obstacleScore;
-		scoreBoard.linkScore = linkScore;
-
-		scoreBoard.flatScore = flatScore;
-		scoreBoard.average = average;
-		scoreBoard.heightError = heightError;
-		scoreBoard.triggerHeight = triggerHeight;
-
-		scoreBoard.obstacle = data.obstacle;
-
-		scoreBoard.attackScore = attackScore;
-		scoreBoard.maxScore = maxScore;
-
-		scoreBoard.trashScore = trashScore;
-
-		scoreBoard.score = score;
-
-		return score;
-	}
 
 };
--- CodevsForStudent/Evaluation.hpp
+++ CodevsForStudent/Evaluation.hpp
@@ -5,71 +5,85 @@
 class Evaluation {
 public:
 
-	const int getScore(const StageArray& _stage, const int _obstacle, const int _attackScore, const int turn) {
-		obstacle = _obstacle;
+	const int getScore(const StageArray& _stage, const int _obstacleNumber, const int _attackScore, const int turn) {
+		obstacleNumber = _obstacleNumber;
 		attackScore = _attackScore;
 
+		searchBlockHeight(_stage);
+		addObstacleHeight(_stage);
+		searchLinkNumber(_stage);
+
+		evaluationBlockFlat(_stage);
+		evaluationShapeError(_stage);
+
+		searchChain(_stage);
+
+		setTotalScore();
+
+		return totalScore;
 	}
 
 	void show() const {
-		cerr << "お邪魔配置スコア\t:" << obstacleScore << endl;
-		cerr << "結合スコア\t\t:" << linkScore << endl << endl;
+		cerr << "お邪魔配置スコア\t:" << obstacleHeightSum << endl;
+		cerr << "結合スコア\t\t:" << blockLink << endl << endl;
 
-		cerr << "凹凸スコア\t\t:" << flatScore << endl;
-		cerr << "ブロックの高さ平均\t:" << average << endl;
-		cerr << "U字形状スコア\t\t:" << heightError << endl;
+		cerr << "凹凸スコア\t\t:" << blockFlatScore << endl;
+		cerr << "ブロックの高さ平均\t:" << blockHeightAverage << endl;
+		cerr << "U字形状スコア\t\t:" << shapeError << endl;
 		cerr << "発火位置\t\t:" << triggerHeight << endl << endl;
 
-		cerr << "お邪魔ブロック数\t:" << obstacle << endl << endl;
+		cerr << "お邪魔ブロック数\t:" << obstacleNumber << endl << endl;
 
-		cerr << "想定連鎖スコア\t\t:" << maxScore << endl;
+		cerr << "想定連鎖スコア\t\t:" << chainScore << endl;
 		cerr << "発火連鎖スコア\t\t:" << attackScore << endl << endl;
 
-		cerr << "連鎖尾スコア\t\t:" << trashScore << endl << endl;
+		cerr << "連鎖尾スコア\t\t:" << trashNumber << endl << endl;
 
-		//cerr << "総合スコア\t\t:" << score << endl;
-		cerr << waitPoint << ", wait:" << waitNumber << endl;
+		cerr << "総合スコア\t\t:" << totalScore << endl;
+		cerr << triggerPoint << ", wait:" << triggerBlock << endl;
 	}
 
 private:
 
 	//ステージ上のお邪魔ブロックの高さの総和
-	int obstacleScore = 0;
+	int obstacleHeightSum = 0;
 	//10未満で繋がったブロック数
-	int linkScore = 0;
+	int blockLink = 0;
 
 	//ブロックの山谷の数
-	int flatScore = 0;
+	int blockFlatScore = 0;
 	//ブロックの高さの平均
-	int average = 0;
-	//ブロックの形状
-	int heightError = 0;
+	int blockHeightAverage = 0;
+	//ブロックの形状誤差
+	int shapeError = 0;
 
 	//発火点の高さ
 	int triggerHeight = 0;
 
 	//お邪魔ブロックの数
-	int obstacle = 0;
+	int obstacleNumber = 0;
 
 	//組まれている連鎖のスコア
-	int maxScore = 0;
+	int chainScore = 0;
 	//発火した時のスコア
 	int attackScore = 0;
 
 	//ごみの数
-	int trashScore = 0;
+	int trashNumber = 0;
 
+	//総合スコア
+	int totalScore = 0;
 	//発火するブロック
-	int waitNumber = 0;
+	int triggerBlock = 0;
 	//発火する座標
-	Point waitPoint;
+	Point triggerPoint;
 
 	//詰みあがったブロックの数
 	array<int, StageWidth> blockTop;
 
 	//ブロックの天辺の検索
 	//「下から」の高さを得る
-	void searchBlockTop(const StageArray& stage) {
+	void searchBlockHeight(const StageArray& stage) {
 
 		blockTop.fill(0);
 
@@ -85,30 +99,30 @@ private:
 			}
 		}
 
-		average = 0;
-		for (const auto& v : blockTop) average += v;
-		average /= (int)blockTop.size();
+		blockHeightAverage = 0;
+		for (const auto& v : blockTop) blockHeightAverage += v;
+		blockHeightAverage /= (int)blockTop.size();
 	}
 	//お邪魔ブロックの高さの総和を得る
-	void searchObstacleBlock(const StageArray& stage) {
+	void addObstacleHeight(const StageArray& stage) {
 
-		obstacleScore = 0;
+		obstacleHeightSum = 0;
 
 		for (int x = 0; x < stage.getWidth(); x++)
 		{
 			for (int y = stage.getHeight() - 1; y >= 0; y--)
 			{
 				if (stage[y][x] == ObstacleBlock)
-					obstacleScore += stage.getHeight() - y;//下からの高さ
+					obstacleHeightSum += stage.getHeight() - y;//下からの高さ
 				else if (stage[y][x] == EmptyBlock)
 					break;
 			}
 		}
 	}
 	//10未満で繋がったブロック数を得る
-	void searchLink(const StageArray& stage) {
+	void searchLinkNumber(const StageArray& stage) {
 
-		linkScore = 0;
+		blockLink = 0;
 
 		//右, 左下, 下, 右下
 		const Point direction[] = { Point(1,0),Point(-1,1),Point(0,1),Point(1,1) };
@@ -133,7 +147,7 @@ private:
 						add += stage[p];
 
 						if (add < AddScore)
-							linkScore++;
+							blockLink++;
 						else
 							break;
 					}
@@ -145,9 +159,9 @@ private:
 
 	//ブロックの凹凸を評価する
 	//1ブロックの山谷の数を得る
-	void evaluationFlat(const StageArray& stage) {
+	void evaluationBlockFlat(const StageArray& stage) {
 
-		flatScore = 0;
+		blockFlatScore = 0;
 
 		const int sub = 4;
 
@@ -160,26 +174,26 @@ private:
 			const int rsub = blockTop[x] - r;
 
 			if (lsub >= sub && rsub >= sub)
-				flatScore++;
+				blockFlatScore++;
 
 			if (lsub <= -sub && rsub <= -sub)
-				flatScore++;
+				blockFlatScore++;
 
 		}
 	}
 	//形がU字型に似ているかを評価する
-	void evaluationShape(const StageArray& stage) {
+	void evaluationShapeError(const StageArray& stage) {
 
-		heightError = 0;
+		shapeError = 0;
 
 		for (int x = 0; x < (int)blockTop.size(); x++)
 		{
-			const int h = ((x - StageWidth / 2)*(x - StageWidth / 2) / 5) + (average - 2);
+			const int h = ((x - StageWidth / 2)*(x - StageWidth / 2) / 5) + (blockHeightAverage - 2);
 			const int sub = h - blockTop[x];
-			heightError += sub*sub;
+			shapeError += sub*sub;
 		}
 
-		heightError = int(sqrt(heightError));
+		shapeError = int(sqrt(shapeError));
 
 	}
 
@@ -187,7 +201,7 @@ private:
 	//ここがメイン評価になる
 	void searchChain(const StageArray& stage) {
 
-		maxScore = 0;
+		chainScore = 0;
 		triggerHeight = 0;
 
 		Simulator simulator;
@@ -199,7 +213,6 @@ private:
 		StageArray maxStage;
 		for (int x = 0; x < (int)blockTop.size(); x++)
 		{
-			//for (int y = 0; y < PackSize; y++)
 			for (int y = 0; y < PackSize; y++)
 			{
 				const Point point = Point(x, stage.getHeight() - blockTop[x] - 1 - y);
@@ -220,19 +233,38 @@ private:
 						StageArray checkStage = stage;
 						checkStage[point] = num;
 						simulator.next(checkStage, s);
-						if (maxScore < s)
+						if (chainScore < s)
 						{
-							maxScore = s;
-							triggerHeight = abs(blockTop[x] + y - average);
+							chainScore = s;
+							triggerHeight = abs(blockTop[x] + y - blockHeightAverage);
 							maxStage = move(checkStage);
-							waitNumber = num;
-							waitPoint = point;
+							triggerBlock = num;
+							triggerPoint = point;
 						}
 					}
 
 				}
 			}
 		}
+
+	}
+
+	void setTotalScore() {
+
+		totalScore -= obstacleHeightSum;
+		totalScore += blockLink;
+
+		totalScore += blockFlatScore;
+		totalScore -= shapeError * 5;
+		totalScore -= triggerHeight * 100;
+
+		totalScore -= obstacleNumber * 10;
+
+		totalScore += chainScore * 10;
+		//相手と自分の状況をみて閾値を決める
+		totalScore += attackScore < 250 ? attackScore * 10 : attackScore * 20;
+
+		totalScore -= chainScore < 250 ? 0 : trashNumber * 3;
 
 	}
 
